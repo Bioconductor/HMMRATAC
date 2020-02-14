8f11e33240021183d6cb211fa874dddd8fdd62c9
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2020-02-14 15:37:35 -0500

    Fixes to Tfbs portion of script

--- inst/script/hyper_script.R
+++ inst/script/hyper_script.R
@@ -24,24 +24,68 @@ siteseqs <- bplapply(seq_along(chrs), function(i) {
     as(seqs, "GRanges")
 })
 
+## Step 2: Perform ORA to find signficant TFBS's
+run_seq3 <- function(seqname, motif_id, pwms, bsgenome, peaks, promoters) {
+    message("Motif ID: ", motif_id, " -- Seqname: ", seqname)
+    ## Find binding sites on chromosome
+    siteseq <- searchSeq(pwms[motif_id], bsgenome[[seqname]], seqname = seqname, min.score="80%", strand = "*")
+    tfbs <- as(siteseq, "GRanges")
+    promoters_chr <- promoters[seqnames(promoters) == seqname]
+    peaks_chr <- peaks[seqnames(peaks) == seqname]
+
+    ## Get promoters with TF
+    idx <- promoters_chr %over% tfbs
+    promoters_with_tf <- promoters_chr[idx]
+    promoters_without_tf <- promoters_chr[!idx]
+
+    ## Summary statistics for phyper
+    npromoters_with_tf <- sum(countOverlaps(promoters_chr, tfbs) > 0)
+    npromoters_without_tf <- length(promoters_chr) - npromoters_with_tf
+    
+    npromoters_with_tf_in_open_chromatin <- sum(countOverlaps(peaks_chr, promoters_with_tf) > 0)
+    npromoters_without_tf_in_open_chromatin <- sum(countOverlaps(peaks_chr, promoters_without_tf) > 0)
+    
+    ## Return values
+    c(q = npromoters_with_tf_in_open_chromatin,
+      m = npromoters_with_tf,
+      n = npromoters_without_tf,
+      k = npromoters_with_tf_in_open_chromatin + npromoters_without_tf_in_open_chromatin)
+}
+
+perform_run_by_motif <- function(motif_id, pwm, bs, peaks, pro) {
+    seqnames <- paste0('chr', 1:22)
+    motif_result <- lapply(seqnames, run_seq3, motif_id = motif_id, pwms = pwm, bsgenome = bs, peaks = peaks, promoters = pro)
+    red <- Reduce(`+`, motif_result)
+    do.call(phyper, as.list(red))
+}
+
+
+motif_names <- head(names(pwm), 16)
+total_results <- bplapply(motif_names, perform_run_by_motif, pwm = pwm, bs = bs, peaks = peaks, pro = pro)
+
+##    c(q = npromoters_with_tf_in_open_chromatin, ## drawn white balls = promoters with TF in open chromatin
+##      m = npromoters_with_tf, ## total white = promoters with TF
+##      n = npromoters_without_tf, ## total black = promoters without TF
+##      k = npromoters_with_tf_in_open_chromatin + npromoters_without_tf_in_open_chromatin) ## all promoters within open chromatin
 ## Step 2: Match motifs to peaks mapping and motifs to promoters
 
 pro <- promoters(TxDb.Hsapiens.UCSC.hg19.knownGene)
 ipro <- ranges(pro)
 peaks <- import.bed('/Users/da42327_ca/Monocytes/01_results/NA_peaks.gappedPeak')
 ipeaks <- ranges(peaks)
-imotifs <- ranges(siteseqs)
+imotifs <- ranges(iteseqs)
 
 motif_to_peak_mappings <- bplapply(siteseqs, function(site) {
     subsetByOverlaps(site, peaks)
 })
 
 site_to_pro_counts <- bplapply(motif_to_peak_mappings, function(mot) {
-    table(countOverlaps(mot, pro) > 0)
-    
+    table(countOverlaps(mot, pro) > 0)    
 })
 
-## Step 3: perform phyper and aquire pvalues for each TFBS
+pvals <- bplapply(seq_along(motif_to_peak_mappings), function(i){
+    phyper(motif
+})
 
 ## Step 4: Download KEGG pathways and aquire gene transcripts
 
--- inst/script/match_to_gene.R
+++ inst/script/match_to_gene.R
@@ -1,7 +1,12 @@
 
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(BSgenome.Hsapiens.UCSC.hg19)
 
 ## Read in pvalues for TFs
+## pvals <- read.delim('pvalues_file.txt')
 
 ## Find which Genes have promoters with enriched TF's and open chromatin regions
+chromosome_locs <- transcripts(TxDb.Hsapiens.UCSC.hg19.knownGene)
+promoters <- getPromoterSeq(chromosome_locs, Hsapiens, upstream=2000, downstream=2000)
 
 ## Functional analysis to find (KEGG) pathways that are enriched
